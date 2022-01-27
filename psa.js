8778fc5f06c04136aad75c0e0f60c0f1cc17c856
pipythonmc <>
2022-01-27 06:13:47 +0000

    More progress, basic fps counter and debug mode

--- psa.js
+++ psa.js
@@ -4,11 +4,22 @@
 // Wrap everything so global scope remains clean
 (() => {
     'use strict';
+    
+    const debug = new URL(document.location.href).searchParams.get('debug') || false;
 
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
-    const state = {};
+    const state = {
+        fps: {
+            lastsecond: 0,
+            fps: 0,
+            tickingfps: 0,
+        },
+        current_slide: 0,
+        animation_state: 0,
+        animation_progress: 0,
+    };
 
     const updateSize = () => {
         canvas.width = document.documentElement.clientWidth;
@@ -25,10 +36,27 @@
         ctx.font = '48px serif';
         ctx.fillText('Hello, World!', 10, 50);
         ctx.fillText(state.i.toString(), 50, 200);
+
+        if (debug) {
+            // Draw FPS counter
+            ctx.fillStyle = 'rgb(255, 255, 255)';
+            ctx.font = '48px monospace';
+            ctx.fillText('FPS: ' + state.fps.fps, 25, canvas.height - 25);
+        }
     };
 
     const update = () => {
         state.i = state.i + 1 || 1;
+
+        if (debug) {
+            state.fps.tickingfps += 1;
+            if (state.fps.lastsecond != Math.floor(Date.now() / 1000)) {
+                state.fps.lastsecond = Math.floor(Date.now()/1000);
+                state.fps.fps = state.fps.tickingfps;
+                state.fps.tickingfps = 0;
+            }
+        }
+
         draw();
         requestAnimationFrame(update);
     }
